9fb106c4c00999da2896b4c8b61b862e5d0e4f91
sabrinabutton <43886209+sabrinabutton@users.noreply.github.com>
2019-02-14 16:24:11 -0500

    Update Microbit_Radio_Send.js

--- Microbit_Radio_Send.js
+++ Microbit_Radio_Send.js
@@ -1,1 +1,12 @@
 //for microbit to send radio to other microbit which then talks to arduino
+input.onButtonPressed(Button.A, function () {
+    radio.sendString("L")
+})
+input.onButtonPressed(Button.B, function () {
+    radio.sendString("R")
+})
+input.onButtonPressed(Button.AB, function () {
+    if (true) {
+    	
+    }
+})

81c40424107e91602d6b5d8b2fb16bc3ca11a755
Sabrina <43886209+sabrinabutton@users.noreply.github.com>
2019-02-15 22:57:09 -0500

    Update Microbit_Radio_Send.js

--- Microbit_Radio_Send.js
+++ Microbit_Radio_Send.js
@@ -1,12 +1,31 @@
-//for microbit to send radio to other microbit which then talks to arduino
-input.onButtonPressed(Button.A, function () {
-    radio.sendString("L")
+/**********************************************************************************************
+*   Program for Microbit to Send information to another Microbit to communicate with Arduino  *
+***********************************************************************************************/
+//set variable "motor_on" which checks whether the robot's motors are running to 0, indicating it is not on
+let motor_on = 0
+
+//when A and B are pressed... 
+input.onButtonPressed(Button.AB, function () {
+    //if the motor is off
+    if (motor_on = 0) {
+        //tell other microbit to send "G" for go
+        radio.sendString("G")
+    }
+    //if the motor is on
+    if (motor_on = 1) {
+        //tell other microbit to send "G" for stop
+        radio.sendString("S")
+    }
 })
+
+//if A is pressed (left button)
 input.onButtonPressed(Button.B, function () {
+    //tell other microbit to send "R" for right
     radio.sendString("R")
 })
-input.onButtonPressed(Button.AB, function () {
-    if (true) {
-    	
-    }
+
+//if B is pressed (right button)
+input.onButtonPressed(Button.A, function () {
+    //tell other microbit to send "L" for left
+    radio.sendString("L")
 })

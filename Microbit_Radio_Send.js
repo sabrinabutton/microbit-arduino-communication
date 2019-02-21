abe402ee2fd990e96a8df7c7dec4759ff1cc45d7
Sabrina <43886209+sabrinabutton@users.noreply.github.com>
2019-02-21 15:58:24 -0500

    Update Microbit_Radio_Send.js

--- Microbit_Radio_Send.js
+++ Microbit_Radio_Send.js
@@ -1,35 +1,50 @@
 /**********************************************************************************************
 *   Program for Microbit to Send information to another Microbit to communicate with Arduino  *
 ***********************************************************************************************/
-//set variable "motor_on" which checks whether the robot's motors are running to 0, indicating it is not on
-let motor_on = 0
-
-//when A and B are pressed... 
-input.onButtonPressed(Button.AB, function () {
-    //if the motor is off
-    if (motor_on = 0) {
-        //tell other microbit to send "G" for go
-        radio.sendString("G")
-        //set motor to on
-        motor_on = 1
-    }
-    //if the motor is on
-    if (motor_on = 1) {
-        //tell other microbit to send "G" for stop
-        radio.sendString("S")
-        //set motor to off
-        motor_on = 0
-    }
+input.onGesture(Gesture.Shake, function () {
+    // tell other microbit to send "G" for go
+    radio.sendString("G")
+    basic.showLeds(`
+        . . . . .
+        . . . . #
+        . . . # .
+        # . # . .
+        . # . . .
+        `)
 })
-
-//if A is pressed (left button)
+// if A is pressed (left button)
 input.onButtonPressed(Button.B, function () {
-    //tell other microbit to send "R" for right
+    // tell other microbit to send "R" for right
     radio.sendString("R")
+    basic.showLeds(`
+        . . # . .
+        . . . # .
+        # # # # #
+        . . . # .
+        . . # . .
+        `)
 })
-
-//if B is pressed (right button)
+// when A and B are pressed...
+input.onButtonPressed(Button.AB, function () {
+    // tell other microbit to send "G" for stop
+    radio.sendString("S")
+    basic.showLeds(`
+        # . . . #
+        . # . # .
+        . . # . .
+        . # . # .
+        # . . . #
+        `)
+})
+// if B is pressed (right button)
 input.onButtonPressed(Button.A, function () {
-    //tell other microbit to send "L" for left
+    // tell other microbit to send "L" for left
     radio.sendString("L")
+    basic.showLeds(`
+        . . # . .
+        . # . . .
+        # # # # #
+        . # . . .
+        . . # . .
+        `)
 })

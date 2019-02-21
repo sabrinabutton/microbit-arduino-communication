e0c46b89b1cbbb10b623846b061f91f88a1469a0
Sabrina <43886209+sabrinabutton@users.noreply.github.com>
2019-02-21 16:07:19 -0500

    Update Microbit_UART_Send_Radio_Recieve.js

--- Microbit_UART_Send_Radio_Recieve.js
+++ Microbit_UART_Send_Radio_Recieve.js
@@ -3,30 +3,12 @@
 ***********************************************************************************************/
 // serial setup...
 serial.redirect(
-    SerialPin.P0,
-    SerialPin.P1,
-    BaudRate.BaudRate9600
+SerialPin.P0,
+SerialPin.P1,
+BaudRate.BaudRate9600
 )
-// when Microbit recieves string from other Microbit...
+// when Microbit recieves string from other
 radio.onReceivedString(function (receivedString) {
-    // if Microbit recieves "G"
-    if (receivedString = "G") {
-        //write "G" to serial
-        serial.writeString("G")
-    }
-    // if Microbit recieves "S"
-    if (receivedString = "S") {
-        //write "S" to serial
-        serial.writeString("S")
-    }
-    // if Microbit recieves "L"
-    if (receivedString = "L") {
-        //write "L" to serial
-        serial.writeString("L")
-    }
-    // if Microbit recieves "R"
-    if (receivedString = "R") {
-        //write "r" to serial
-        serial.writeString("R")
-    }
+    // write recievedString to serial
+    serial.writeString(receivedString)
 })

24204d645af2db32f35c06bc3169c99782e27148
Sabrina <43886209+sabrinabutton@users.noreply.github.com>
2019-02-28 16:25:28 -0500

    Update Arduino_UART_Recieve.ino

--- Arduino_UART_Recieve.ino
+++ Arduino_UART_Recieve.ino
@@ -6,9 +6,32 @@
 #define OLED_RESET 4
 #define SSD1306_LCDHEIGHT 128
 #define SSD1306_LCDWIDTH  64
+
+
+//Define the states of the machine
+#define STOP_S 0
+#define WAIT_STOP_S 1
+#define FORWARD_S 2
+#define MOVE_FORWARD_S 3
+#define LEFT_S 4
+#define TURN_LEFT_S 5
+#define RIGHT_S 6
+#define TURN_RIGHT_S 7
+
+//MOTOR CONTROLLER
+#define dirBPin 4 // direction //R
+#define pwmBPin 3 // speed
+#define dirAPin  12 // direction //L
+#define pwmAPin  11 // speed
+MotorController mc(dirAPin, pwmAPin, dirBPin, pwmBPin, 1, 1);
 Adafruit_SSD1306 display(OLED_RESET);
 
 char motion_command;
+
+uint8_t fsm_state = STOP_S;
+int move_cnt;
+
+#define runEvery(t) for (static typeof(t) _lasttime;(typeof(t))((typeof(t))millis() - _lasttime) > (t);_lasttime += (t))
 
 void setup() {
   
@@ -23,14 +46,80 @@ void setup() {
   display.print("ON");
   display.clearDisplay();
   display.display();
+  Serial.println(F("Exp 0.1"));
 
 }
 
 void loop() {
+
+  runEvery(50) {
+      //state machine
+      switch (fsm_state)
+      {
+        case STOP_S:  //Statements to execute
+          move_cnt = 100;
+          mc.move(0,0);
+          fsm_state = WAIT_STOP_S;
+          Serial.println(F("Stop"));
+
+          break;
+        case WAIT_STOP_S:
+          move_cnt--;
+          if(move_cnt == 0) {
+             fsm_state = FORWARD_S;
+          }
+          break;
+        case FORWARD_S:   //Statements to execute
+          move_cnt = 100;
+          mc.move(255,255);
+          fsm_state = MOVE_FORWARD_S;
+          Serial.println(F("Go"));
+
+          break;
+        case MOVE_FORWARD_S:   //Statements to execute
+          move_cnt--;
+          // if == zero - then go forward
+          if(move_cnt == 0) {
+             fsm_state = STOP_S;
+          }
+          break;
+        case LEFT_S:
+          move_cnt = 100;
+          mc.move(255,0);
+          fsm_state = TURN_LEFT_S;
+          Serial.println(F("LEFT"));
+
+          break;
+        case TURN_LEFT_S:
+          // decrement turn counter
+          move_cnt--;
+          // if == zero - then go forward
+          if(move_cnt == 0) {
+             fsm_state = STOP_S;
+          } 
+         break;
+        case RIGHT_S:
+          move_cnt = 100;
+          mc.move(0,255);
+          fsm_state = TURN_RIGHT_S;
+          Serial.println(F("RIGHT"));
+
+          break;
+        case TURN_RIGHT_S:
+          // decrement turn counter
+          move_cnt--;
+          // if == zero - then go forward
+          if(move_cnt == 0) {
+             fsm_state = FORWARD_S;
+          } 
+         break;
+       default:
+         break;
+      }
+     
+  }
   
-  int i = 0;
-  
-  display.clearDisplay();
+  /*display.clearDisplay();
     display.setCursor(0, 0);
   display.println("Waiting for Serial...");
     display.display();
@@ -42,40 +131,35 @@ void loop() {
     display.println("Waiting for String...");
       display.display();
     delay(2500); //allows all serial sent to be received together
-    while (Serial.available() && i < 1) 
-    {
+    
       motion_command = Serial.read();
       
-      if (motion_command == "G")
+      if (motion_command == 'G')
       {
         
       }
-      else if (motion_command == "S")
+      else if (motion_command == 'S')
       {
         
       }
-      else if (motion_command == "L")
+      else if (motion_command == 'L')
       {
         
       }
-      else if (motion_command == "R")
+      else if (motion_command == 'R')
       {
         
       }
       else 
       {
-        motion_command = "Error";
+        motion_command = 'E';
       }
-    
-  }   
 
-  if (i > 0) 
-  {
       display.setCursor(0, 40);
         display.clearDisplay();
-    display.println("Command recieved!", motion_command);
+    display.print("Command recieved: ");
+    display.println(motion_command);
       display.display();
       delay(3000);
-  }
-  
+ }*/
 }

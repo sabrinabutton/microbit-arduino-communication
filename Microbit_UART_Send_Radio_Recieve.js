aacc1a8f407957aaedcd3b7838ec0bb4a2497a87
Sabrina <43886209+sabrinabutton@users.noreply.github.com>
2019-04-25 15:52:39 -0400

    Update Microbit_UART_Send_Radio_Recieve.js

--- Microbit_UART_Send_Radio_Recieve.js
+++ Microbit_UART_Send_Radio_Recieve.js
@@ -1,14 +1,64 @@
 /**********************************************************************************************
 *   Program for Microbit to Send information to the Arduino for Movement                      *
 ***********************************************************************************************/
-// serial setup...
-serial.redirect(
-SerialPin.P0,
-SerialPin.P1,
-BaudRate.BaudRate9600
-)
 // when Microbit recieves string from other
 radio.onReceivedString(function (receivedString) {
     // write recievedString to serial
-    serial.writeString(receivedString)
+    // serial.writeString(receivedString)
+    if (receivedString == "G") {
+        // show user
+        basic.showLeds(`
+            . . . . .
+            . . . . #
+            . . . # .
+            # . # . .
+            . # . . .
+            `)
+        // pause and clear
+        basic.pause(300)
+        basic.clearScreen()
+    } else if (receivedString == "S") {
+        // show user
+        basic.showLeds(`
+            # . . . #
+            . # . # .
+            . . # . .
+            . # . # .
+            # . . . #
+            `)
+        // pause and clear
+        basic.pause(300)
+        basic.clearScreen()
+    } else if (receivedString == "L") {
+        // show user
+        basic.showLeds(`
+            . . # . .
+            . # . . .
+            # # # # #
+            . # . . .
+            . . # . .
+            `)
+        // pause and clear
+        basic.pause(300)
+        basic.clearScreen()
+    } else if (receivedString == "R") {
+        // show user
+        basic.showLeds(`
+            . . # . .
+            . . . # .
+            # # # # #
+            . . . # .
+            . . # . .
+            `)
+        // pause and clear
+        basic.pause(300)
+        basic.clearScreen()
+    }
 })
+radio.setGroup(1)
+// serial setup...
+serial.redirect(
+    SerialPin.P0,
+    SerialPin.P1,
+    BaudRate.BaudRate9600
+)

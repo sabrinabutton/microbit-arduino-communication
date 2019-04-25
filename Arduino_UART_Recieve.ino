26bc713fa9dbf3347cce44e21d70417aed1ca297
Sabrina <43886209+sabrinabutton@users.noreply.github.com>
2019-04-25 16:07:36 -0400

    Update Arduino_UART_Recieve.ino

--- Arduino_UART_Recieve.ino
+++ Arduino_UART_Recieve.ino
@@ -51,115 +51,116 @@ void setup() {
 }
 
 void loop() {
-
-  runEvery(50) {
-      //state machine
-      switch (fsm_state)
-      {
-        case STOP_S:  //Statements to execute
-          move_cnt = 100;
-          mc.move(0,0);
-          fsm_state = WAIT_STOP_S;
-          Serial.println(F("Stop"));
-
-          break;
-        case WAIT_STOP_S:
-          move_cnt--;
-          if(move_cnt == 0) {
-             fsm_state = FORWARD_S;
-          }
-          break;
-        case FORWARD_S:   //Statements to execute
-          move_cnt = 100;
-          mc.move(255,255);
-          fsm_state = MOVE_FORWARD_S;
-          Serial.println(F("Go"));
-
-          break;
-        case MOVE_FORWARD_S:   //Statements to execute
-          move_cnt--;
-          // if == zero - then go forward
-          if(move_cnt == 0) {
-             fsm_state = STOP_S;
-          }
-          break;
-        case LEFT_S:
-          move_cnt = 100;
-          mc.move(255,0);
-          fsm_state = TURN_LEFT_S;
-          Serial.println(F("LEFT"));
-
-          break;
-        case TURN_LEFT_S:
-          // decrement turn counter
-          move_cnt--;
-          // if == zero - then go forward
-          if(move_cnt == 0) {
-             fsm_state = STOP_S;
-          } 
-         break;
-        case RIGHT_S:
-          move_cnt = 100;
-          mc.move(0,255);
-          fsm_state = TURN_RIGHT_S;
-          Serial.println(F("RIGHT"));
-
-          break;
-        case TURN_RIGHT_S:
-          // decrement turn counter
-          move_cnt--;
-          // if == zero - then go forward
-          if(move_cnt == 0) {
-             fsm_state = FORWARD_S;
-          } 
-         break;
-       default:
-         break;
-      }
-     
-  }
-  
-  /*display.clearDisplay();
-    display.setCursor(0, 0);
-  display.println("Waiting for Serial...");
-    display.display();
-      delay(1000);
-  
+    
   if (Serial.available()) 
   {
-      display.setCursor(0, 20);
+      /*display.setCursor(0, 20);
     display.println("Waiting for String...");
       display.display();
-    delay(2500); //allows all serial sent to be received together
+    delay(2500); //allows all serial sent to be received together*/
     
       motion_command = Serial.read();
       
       if (motion_command == 'G')
       {
-        
+        fsm_state = FORWARD_S;
       }
       else if (motion_command == 'S')
       {
-        
+        fsm_state = STOP_S;
       }
       else if (motion_command == 'L')
       {
-        
+        fsm_state = TURN_LEFT_S;
       }
       else if (motion_command == 'R')
       {
-        
+        fsm_state = TURN_RIGHT_S;
       }
       else 
       {
         motion_command = 'E';
+        fsm_state = STOP_S;
       }
 
-      display.setCursor(0, 40);
-        display.clearDisplay();
-    display.print("Command recieved: ");
+      Serial.setCursor(0, 40);
+        Serial.clearDisplay();
+    Serial..print("Command recieved: ");
     display.println(motion_command);
       display.display();
       delay(3000);
- }*/
+ }
+ 
+ // runEvery(50) {
+      //state machine
+      switch (fsm_state)
+      {
+        case STOP_S:  //Statements to execute
+          //move_cnt = 100;
+          mc.move(0,0);
+          //fsm_state = WAIT_STOP_S;
+          //Serial.println(F("Stop"));
+
+          break;
+        /*case WAIT_STOP_S:
+         // move_cnt--;
+          //if(move_cnt == 0) {
+            // fsm_state = FORWARD_S;
+         // }
+          break;*/
+        case FORWARD_S:   //Statements to execute
+          //move_cnt = 100;
+          mc.move(255,255);
+          //fsm_state = MOVE_FORWARD_S;
+          //Serial.println(F("Go"));
+
+          break;
+        /*case MOVE_FORWARD_S:   //Statements to execute
+          move_cnt--;
+          // if == zero - then go forward
+          if(move_cnt == 0) {
+             fsm_state = STOP_S;
+          }
+          break;*/
+        case LEFT_S:
+          //move_cnt = 100;
+          mc.move(255,0);
+          //fsm_state = TURN_LEFT_S;
+          //Serial.println(F("LEFT"));
+
+          break;
+        /*case TURN_LEFT_S:
+          // decrement turn counter
+          move_cnt--;
+          // if == zero - then go forward
+          if(move_cnt == 0) {
+             fsm_state = STOP_S;
+          } 
+         break;*/
+        case RIGHT_S:
+          //move_cnt = 100;
+          mc.move(0,255);
+          //fsm_state = TURN_RIGHT_S;
+          //Serial.println(F("RIGHT"));
+
+          break;
+        /*case TURN_RIGHT_S:
+          // decrement turn counter
+          move_cnt--;
+          // if == zero - then go forward
+          if(move_cnt == 0) {
+             fsm_state = FORWARD_S;
+          } 
+         break;*/
+      
+      }
+     
+ // }
+  
+  /*display.clearDisplay();
+    display.setCursor(0, 0);
+  display.println("Waiting for Serial...");
+    display.display();
+      delay(1000);*/
+
 }

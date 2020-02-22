d9f937075f5761863865a46c85809658127241e6
Sabrina Button <43886209+sabrinabutton@users.noreply.github.com>
2020-02-22 11:16:20 -0500

    Update Microbit_UART_Send_Radio_Recieve.js

--- Microbit_UART_Send_Radio_Recieve.js
+++ Microbit_UART_Send_Radio_Recieve.js
@@ -1,10 +1,17 @@
 /**********************************************************************************************
 *   Program for Microbit to Send information to the Arduino for Movement                      *
 ***********************************************************************************************/
+radio.setGroup(1)
+// serial setup...
+serial.redirect(
+    SerialPin.P0,
+    SerialPin.P1,
+    BaudRate.BaudRate9600
+)
 // when Microbit recieves string from other
 radio.onReceivedString(function (receivedString) {
     // write recievedString to serial
-    // serial.writeString(receivedString)
+    serial.writeString(receivedString)
     if (receivedString == "G") {
         // show user
         basic.showLeds(`
@@ -55,10 +62,4 @@ radio.onReceivedString(function (receivedString) {
         basic.clearScreen()
     }
 })
-radio.setGroup(1)
-// serial setup...
-serial.redirect(
-    SerialPin.P0,
-    SerialPin.P1,
-    BaudRate.BaudRate9600
-)
+
